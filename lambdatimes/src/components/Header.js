609eeb4013cadac7dc8df3c071c75bcc89d657f4
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-14 11:03:57 +0100

    styled componets added to header

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
-const Header = () => {
+export const Header = () => {
   return (
     <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
+      <Date>SMARCH 32, 2018</Date>
       <h1>Lambda Times</h1>
       <span className="temp">98°</span>
     </div>
   )
 }
+const Date = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin-left: 25px;
+  flex: 1;
+`
+const 
 
-export default Header
+// export default Header;

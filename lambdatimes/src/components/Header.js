140c27c8f4ca0f850cae73c2b8494356286b4e3f
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-14 14:59:23 +0100

    refactored to styled-components

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
-export const Header = () => {
+const Header = () => {
   return (
     <div className="header">
       <Date>SMARCH 32, 2018</Date>
@@ -20,7 +20,7 @@ const Date = styled.span`
   letter-spacing: 1px;
   margin-left: 25px;
   flex: 1;
-`
-const 
+`;
 
-// export default Header;
+
+export default Header;

626694a2b42ce6d5a9f7d4d31615e20f703795a8
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-14 14:58:43 +0100

    proptypes added to tab.js

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+
 
 const Tab =({ tab, selectTabHandler, selectedTab }) => {
   console.log(selectTabHandler)
   return (
     <div
       className={tab ===selectedTab ? 'tab active-tab' : 'tab'}
-      onClick={() => selectTabHandler()}
+      onClick={() => selectTabHandler(tab)}
     >
       {tab}
     </div>
@@ -13,5 +15,8 @@ const Tab =({ tab, selectTabHandler, selectedTab }) => {
 };
 
 // Make sure you include PropTypes on your props.
+Tab.propTypes = {
+  tabs: PropTypes.string.isRequired
+}
 
 export default Tab;
